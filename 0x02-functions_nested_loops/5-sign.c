5a5bd4016297cd838874b3cbb5ebe05b1126dda8
Y7gaar <ym3gamind10@gmail.com>
2023-10-10 10:54:34 -0700

    :racehorse:

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,8 +2,10 @@
 
 /**
  * print_sign - prints the sign of a number
- * 
- * Return: 1
+ * @n: the int to check
+ * Return: 1 and prints + if n is greater than zero
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if n is less than zero
  */
 int print_sign(int n)
 {
