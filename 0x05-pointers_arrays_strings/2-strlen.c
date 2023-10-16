38bfa953192653735cc2b65a22936428256ace7f
Y7gaar <ym3gamind10@gmail.com>
2023-10-16 08:46:19 -0700

    g

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * int-strlen - a function that returns the length of a string.
+ * @s: string that its length wil be found
+ *
+ * OCT 16th
+ * Ahmad Ali
+ *
+ * Return: Always 0.
+ */
+int _strlen(char *s);
+{
+	int length = 0;
+
+	while (string[length] != '\0')
+	{
+		length++;
+	}
+	return length;
+}
