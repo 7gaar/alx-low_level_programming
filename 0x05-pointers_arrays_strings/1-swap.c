34da27ed96d473ed84f1efe554d8973d54925080
Y7gaar <ym3gamind10@gmail.com>
2023-10-16 08:14:00 -0700

    g

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 /**
+ * main - Write a function that swaps the values of two integers.
+ *
  * swap_int: a function that swaps the values of two integers.
+ * @a: a variable that will be updated
+ * @b: a variable that will be updated
  *
  * OCT 10th 2023
  * Ahmad Ali
@@ -10,6 +14,7 @@
 void swap_int(int *a, int *b)
 {
 	int l;
+
 	l = *a;
 	*a = *b;
 	*b = l;
