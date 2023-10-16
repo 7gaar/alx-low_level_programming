debbe2cbedb4160f6f631947b28f6e26436f11a1
Y7gaar <ym3gamind10@gmail.com>
2023-10-16 09:39:12 -0700

    g

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rev_string - reverses the string
+ * @s: string to be reversed
+ *
+ * OCT 16th 23
+ * Ahmad Ali
+ *
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+	int l = 0, m, n;
+	char o;
+
+	while (s[l] != '\0')
+	{
+		a++;
+	}
+	n = l - 1;
+	for (m = 0; n >= 0 && m < n; n--, m++)
+	{
+		o = s[m];
+		s[m] = s[n];
+		s[n] = o;
+	}
+}
