12c30ac9b6428dba70b889f0017860c079213edb
Y7gaar <ym3gamind10@gmail.com>
2023-10-25 06:55:48 -0700

    g

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,7 +2,7 @@
 
 /**
  * _puts_recursion - prints a string, followed by a new line
- * @s: string to print
+ * @s: a string to be printed
  *
  * OCT 25th 23
  * Ahmed Ali
@@ -11,7 +11,7 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar("\n");
+		_putchar('\n');
 		return;
 	}
 	_putchar(*s);
