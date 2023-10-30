ac05043d54e2d5d86fee616e9a85c712779b7ae8
Y7gaar <ym3gamind10@gmail.com>
2023-10-30 13:05:11 -0700

    g

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -27,9 +27,9 @@ char *str_concat(char *s1, char *s2)
 	if (ptr == 0)
 		return (0);
 
-	for (i = 0; i < length1; i++)
+	for (i = 0; i < len1; i++)
 		ptr[i] = s1[i];
-	for (j = 0; j < length2; j++)
+	for (j = 0; j < len2; j++)
 	{
 		ptr[i + j] = s2[j];
 	}
