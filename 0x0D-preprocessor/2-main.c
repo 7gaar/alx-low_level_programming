7832efa7d6831c039805f71a1e2ec92cbdb0299c
Y7gaar <ym3gamind10@gmail.com>
2023-11-06 09:48:34 -0800

    g

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main (void)
+/**
+ * main - printss the name of the file
+ *
+ * NOV 6th 23
+ * Ahemd Ali
+ *
+ * Return: 0
+ */
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
