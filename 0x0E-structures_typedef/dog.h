d3d689155659b83635947d575784c12528dcc76d
Y7gaar <ym3gamind10@gmail.com>
2023-11-06 06:41:40 -0800

    g

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - info for a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Ahmed Ali
+ * NOV 6TH 23
+ */
 struct dog
 {
 	char *name;
