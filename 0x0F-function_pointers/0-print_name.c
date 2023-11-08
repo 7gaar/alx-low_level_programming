b864ba2520e7fc27153649b8546d7aa366b1d5ce
7gaar <a7hmed2li@gmail.c>
2023-11-08 18:51:10 +0300

    f

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,13 @@
+#include "FUN_H"
+/**
+ * print_name - a function that prints a name
+ * @name: name to be printed
+ * @f: a pointe to a function
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == 0 || f == 0)
 		return;
-	
+
 	 f(name);
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef FUNCTION_POINTERS
-#define FUNCTION_POINTERS
+#ifndef FUN_H
+#define FUN_H
 #include <stdio.h>
 
 void print_name(char *name, void (*f)(char *));
