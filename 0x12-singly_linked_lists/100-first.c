6401b750a3909d43cb97e28fef71f5ab586a2719
7gaar <a7hmed2li@gmail.c>
2023-11-17 00:26:58 +0300

    h

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * myStartupFun - Apjhmjm
+ */
+void myStartupFun(void) __attribute__ ((constructor));
+
+/**
+ * myStartupFun - nh
+ */
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
