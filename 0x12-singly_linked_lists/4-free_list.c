0d71086d1dc8c4c9dc751dd8b0d31ab85672a43e
7gaar <a7hmed2li@gmail.c>
2023-11-16 23:29:58 +0300

    f

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -3,11 +3,11 @@
 /**
  * free_list - ajoefm
  * @head: rifoe
- * 
+ *
  * Return: kfmok
 */
 void free_list(list_t *head)
-  {
+{
 	if (head)
 	{
 		free_list(head->next);
@@ -15,4 +15,4 @@ void free_list(list_t *head)
 			free(head->str);
 		free(head);
 	}
-  }
+}
