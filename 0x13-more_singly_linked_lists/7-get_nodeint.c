97c4ef0a963ea48df4c86a2007ae968899499e0d
7gaar <a7hmed2li@gmail.c>
2023-11-20 18:39:24 +0300

    d

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,11 +13,15 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i;
 
 	tmp = head;
+	if (head == NULL)
+		return (0);
 	for (i = 0; i < index; i++)
 	{
 		tmp = tmp->next;
-		if (tmp->next == 0)
+		if (tmp == 0)
+		{
 			return (0);
+		}
 	}
-	return (tmp->next);
+	return (tmp);
 }
