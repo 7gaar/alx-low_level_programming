a51cc363a4a754257f7ef3d727430c9ca194b6f6
7gaar <a7hmed2li@gmail.c>
2023-11-23 17:47:40 +0300

    g

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num, power;
+	int len;
+
+	if (!b)
+		return (0);
+
+	for (len = 0; b[len]; len++)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+	}
+
+	for (power = 1, num = 0, len--; len >= 0; len--, power *= 2)
+	{
+		if (b[len] == '1')
+			num += power;
+	}
+
+	return (num);
+}
