b80c25a2b2966c8bad4bc039e121a09997054896
7gaar <a7hmed2li@gmail.c>
2023-11-27 16:44:59 +0300

    🕳️

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t o, r, num;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	num = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || num == -1 || num != r)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(o);
+
+	return (num);
+}
