310f166ec7a39765ad08b82709eb86a98d1d7260
7gaar <m3gamind10@gmail.com>
2024-02-02 21:37:41 +0300

    d

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,40 +9,42 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *newNode;
-	unsigned long int hash, index;
-	int i;
+	char *value_cp;
+	unsigned long int index, i;
 
-	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
-	    key == NULL || strlen(key) == 0 || value == NULL)
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
+
+	value_cp = strdup(value);
+	if (value_cp == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_cp;
+			return (1);
+		}
+	}
+
 	newNode = malloc(sizeof(hash_node_t));
 	if (newNode == NULL)
+	{
+		free(value_cp);
 		return (0);
+	}
 	newNode->key = strdup(key);
 	if (newNode->key == NULL)
 	{
 		free(newNode);
 		return (0);
 	}
-	newNode->value = strdup(value);
-	if (newNode->value == NULL)
-	{
-		free(newNode);
-		return (0);
-	}
-	newNode->next = NULL;
-	hash = hash_djb2(key);
-	index = key_index(key, ht->size);
-	for (i = index; ht->array[i]; i++)
-	{
-		if (strcmp(ht->array[i]->key, key) == 0)
-		{
-			free(ht->array[i]->value);
-			ht->array[i]->value = newNode->value;
-			return (1);
-		}
-	}
+	newNode->value = value_cp;
 	newNode->next = ht->array[index];
 	ht->array[index] = newNode;
+
 	return (1);
 }
